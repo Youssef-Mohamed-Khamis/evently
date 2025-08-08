36bdcff37029a1225c2ff145c546d40717831b88
Youssef <swimmer.shar12@gmail.com>
2025-08-08 15:57:33 +0300

    + Finish onBoarding Screen Dark Mode

--- lib/ui/login/screen/login_screen.dart
+++ lib/ui/login/screen/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:easy_localization/easy_localization.dart';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -28,25 +27,25 @@ class _LoginScreenState extends State<LoginScreen> {
   late TextEditingController emailController;
   late TextEditingController passwordController;
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     emailController = TextEditingController();
     passwordController = TextEditingController();
   }
+
   @override
   void dispose() {
-    // TODO: implement dispose
-    super.dispose();
     emailController.dispose();
     passwordController.dispose();
-
+    super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     ThemeProvider themeProvider = Provider.of<ThemeProvider>(context);
-    if(context.locale.languageCode=="ar"){
+    if (context.locale.languageCode == "ar") {
       selectedLanguage = 1;
     }
     return Scaffold(
@@ -59,23 +58,23 @@ class _LoginScreenState extends State<LoginScreen> {
               child: Column(
                 children: [
                   Image.asset(AssetsManager.logo),
-                  SizedBox(height: 24,),
+                  const SizedBox(height: 24),
                   CustomField(
                     keyboard: TextInputType.emailAddress,
                     controller: emailController,
                     hint: 'email'.tr(),
                     prefix: AssetsManager.email,
-                    validation: (value){
-                      if(value == null || value.isEmpty){
+                    validation: (value) {
+                      if (value == null || value.isEmpty) {
                         return "shouldEmpty".tr();
                       }
-                      if(!RegExp(emailRegex).hasMatch(value)){
+                      if (!RegExp(emailRegex).hasMatch(value)) {
                         return "Email not valid";
                       }
                       return null;
                     },
                   ),
-                  SizedBox(height: 16,),
+                  const SizedBox(height: 16),
                   CustomField(
                     keyboard: TextInputType.visiblePassword,
                     controller: passwordController,
@@ -83,79 +82,102 @@ class _LoginScreenState extends State<LoginScreen> {
                     prefix: AssetsManager.lock,
                     isPassword: true,
                     validation: (value) {
-                      if(value == null || value.isEmpty){
+                      if (value == null || value.isEmpty) {
                         return "shouldEmpty".tr();
                       }
-                      if(value.length<8){
+                      if (value.length < 8) {
                         return "password mustn't be less than 8 char";
                       }
-
+                      return null;
                     },
                   ),
-                  SizedBox(height: 16,),
+                  const SizedBox(height: 16),
                   Align(
                     alignment: AlignmentDirectional.centerEnd,
                     child: TextButton(
-                        onPressed: (){
-                          Navigator.pushNamed(context, ForgetPasswordScreen.routeName);
-                        },
-                        child: Text("forgetPass".tr(),
-                          style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                              fontSize: 16,
-                              decorationColor: Theme.of(context).colorScheme.primary,
-                              decorationThickness: 2,
-
-                              decoration: TextDecoration.underline
-                          ),)),
+                      onPressed: () {
+                        Navigator.pushNamed(
+                            context, ForgetPasswordScreen.routeName);
+                      },
+                      child: Text(
+                        "forgetPass".tr(),
+                        style:
+                        Theme.of(context).textTheme.titleSmall?.copyWith(
+                          fontSize: 16,
+                          decorationColor:
+                          Theme.of(context).colorScheme.primary,
+                          decorationThickness: 2,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
                   ),
-                  SizedBox(height: 24,),
-                  Container(
+                  const SizedBox(height: 24),
+                  SizedBox(
                     width: double.infinity,
-                    child: CustomButton(title: "login".tr(), onClick: (){
-                    }),
+                    child: CustomButton(
+                      title: "login".tr(),
+                      onClick: () {
+                        if (formKey.currentState!.validate()) {
+                          // لو الفورم صح
+                          print("Form is valid");
+                        }
+                      },
+                    ),
                   ),
-                  SizedBox(height: 24,),
+                  const SizedBox(height: 24),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("dontHaveAccount".tr(),style: Theme.of(context).textTheme.bodySmall,),
+                      Text(
+                        "dontHaveAccount".tr(),
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
                       TextButton(
-                          onPressed: (){
-                            Navigator.pushNamed(context, RegisterScreen.routeName);
-                          },
-                          child: Text("createAcc".tr(),
-                            style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                                fontSize: 16,
-                                decorationColor: Theme.of(context).colorScheme.primary,
-                                decorationThickness: 2,
-
-                                decoration: TextDecoration.underline
-                            ),)),
+                        onPressed: () {
+                          Navigator.pushNamed(
+                              context, RegisterScreen.routeName);
+                        },
+                        child: Text(
+                          "createAcc".tr(),
+                          style:
+                          Theme.of(context).textTheme.titleSmall?.copyWith(
+                            fontSize: 16,
+                            decorationColor:
+                            Theme.of(context).colorScheme.primary,
+                            decorationThickness: 2,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
                     ],
                   ),
-                  SizedBox(height: 24,),
+                  const SizedBox(height: 24),
                   CustomSwitch(
-                      onChange: (value){
-                        setState(() {
-                          selectedLanguage = value;
-                        });
-                        if(selectedLanguage==1){
-                          context.setLocale(Locale("ar"));
-                        }else{
-                          context.setLocale(Locale("en"));
-
-                        }
-                      },
-                      icons:[
-                        SvgPicture.asset(AssetsManager.us,
-                          height: 30,
-                          width: 30,
-                        ),
-                        SvgPicture.asset(AssetsManager.eg, height: 30,
-                          width: 30,),
-                      ] ,
-                      current: selectedLanguage)
-
+                    onChange: (value) {
+                      setState(() {
+                        selectedLanguage = value;
+                      });
+                      if (selectedLanguage == 1) {
+                        context.setLocale(const Locale("ar"));
+                      } else {
+                        context.setLocale(const Locale("en"));
+                      }
+                    },
+                    icons: [
+                      SvgPicture.asset(
+                        AssetsManager.us,
+                        height: 30,
+                        width: 30,
+                      ),
+                      SvgPicture.asset(
+                        AssetsManager.eg,
+                        height: 30,
+                        width: 30,
+                      ),
+                    ],
+                    current: selectedLanguage,
+                  )
                 ],
               ),
             ),
@@ -164,5 +186,4 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
-
 }
